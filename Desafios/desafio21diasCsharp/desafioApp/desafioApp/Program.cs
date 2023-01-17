39ed170792190610c3cb2651239ed81272836384
GabrielInacio03 <inaciogs03@gmail.com>
2023-01-16 23:05:55 -0300

    primeiro commit

---
+++ Desafios/desafio21diasCsharp/desafioApp/desafioApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace console;
+public class Program 
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Olá Mundo");
+		var x = 0;
+		string templatestring =
+			$"""
+			Testando novo template string
+			do dotnet7
+			o valor de x é: {x}
+			""";
+
+		Console.WriteLine(templatestring);
+	}
+	
+}
+
