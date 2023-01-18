1aa4122f02478c07cc83c1b4996d7e66ca2b9347
GabrielInacio03 <inaciogs03@gmail.com>
2023-01-17 21:26:21 -0300

    criação do projeto AppLogica

---
+++ Desafios/logica/AppLogica/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AppLogica // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Desenvolvendo o raciocínio lógico com dotnet7 em aplicação console");
+            Console.WriteLine("===========================================================================");
+            Console.WriteLine("");
+        }
+    }
+}
