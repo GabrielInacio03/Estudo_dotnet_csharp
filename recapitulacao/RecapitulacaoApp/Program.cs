0114760cabea2da1890cb2cffcd1206b499c0794
GabrielInacio03 <inaciogs03@gmail.com>
2023-01-22 21:59:38 -0300

    Operadores comparativos

---
+++ recapitulacao/RecapitulacaoApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RecapitulacaoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            bool cond1 = a < 10; //false
+            bool cond2 = a < 20; //true
+            bool cond3 = a > 10; //false
+            bool cond4 = a > 5;  //true
+
+            Console.WriteLine(cond1);
+            Console.WriteLine(cond2);
+            Console.WriteLine(cond3);
+            Console.WriteLine(cond4);
+            Console.WriteLine("------------------");
+
+            bool cond5 = a <= 10; //true
+            bool cond6 = a >= 10; //true
+            bool cond7 = a == 10; //true
+            bool cond8 = a != 10; //false
+
+            Console.WriteLine(cond5);
+            Console.WriteLine(cond6);
+            Console.WriteLine(cond7);
+            Console.WriteLine(cond8);
+        }
+    }
+}
