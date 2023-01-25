42a506c66419f880cfea30acf7b7c215bd83930e
GabrielInacio03 <inaciogs03@gmail.com>
2023-01-24 22:19:25 -0300

    operadores lógicos

--- recapitulacao/RecapitulacaoApp/Program.cs
+++ recapitulacao/RecapitulacaoApp/Program.cs
@@ -27,6 +27,18 @@ namespace RecapitulacaoApp
             Console.WriteLine(cond6);
             Console.WriteLine(cond7);
             Console.WriteLine(cond8);
+
+            Console.WriteLine("------------------");
+            
+            bool c1 = 2 > 3 || 4 != 5; //true
+            bool c2 = !(2 > 3) && 4 != 5; //true
+            bool c3 = 10 < 5;
+            bool c4 = c1 || c2 && c3; // true
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
+            Console.WriteLine(c4);
+
         }
     }
 }
