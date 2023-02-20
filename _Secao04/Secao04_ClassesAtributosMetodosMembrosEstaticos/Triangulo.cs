32cc381f2f458ec13b72a636f34598fac6276a03
GabrielInacio03 <inaciogs03@gmail.com>
2023-02-20 06:18:32 -0300

    adicionando a classe e seus objetos

---
+++ _Secao04/Secao04_ClassesAtributosMetodosMembrosEstaticos/Triangulo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Secao04_ClassesAtributosMetodosMembrosEstaticos
+{
+    public class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
