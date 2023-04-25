abc1f4f4b671070a3c3ff6fbb42f26fefa1c223a
GabrielInacio03 <inaciogs03@gmail.com>
2023-04-25 20:24:46 -0300

    exercício de fixação, construtores, sobrecarga e encapsulamento

--- _Secao05/exercicioFixacao/Conta.cs
+++ _Secao05/exercicioFixacao/Conta.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,11 +13,29 @@ namespace exercicioFixacao
         public double Saldo { get; set; }
 
         public Conta(){ }
-        public Conta(int numero, string titular, double saldo)
+        public Conta(int numero, string titular, double saldo) :  this()
         {
             this.Numero = numero;
             this.Titular = titular;
             this.Saldo = saldo;
+        }
+        public Conta(int numero, string titular) : this()
+        {
+            this.Numero = numero;
+            this.Titular = titular;
+        }
+
+		public void AdicionarNoSaldo(double valor)
+		{
+			this.Saldo +=  valor;
+		}
+		public void RemoverNoSaldo(double valor)
+		{
+			this.Saldo -=  valor - 5;
+		}
+        public override string ToString()
+        {
+            return string.Format("Conta {0}, Titular: {1}, Saldo: $ {2}", this.Numero, this.Titular, this.Saldo.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
--- _Secao05/exercicioFixacao/Program.cs
+++ _Secao05/exercicioFixacao/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace exercicioFixacao // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================  EXERCÍCIOS DE FIXAÇÃO ================");
+            Conta conta;
+
+            System.Console.Write("Entre com o número da conta: ");
+            int numConta = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine();
+            System.Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            System.Console.WriteLine();
+            System.Console.Write("Haverá depósito inicial (s/n)?");
+            char temDeposito = char.Parse(Console.ReadLine());
+
+            System.Console.WriteLine();
+            double valorSaldo = 0.00;
+            
+            if(temDeposito == 's')
+            {
+                System.Console.Write("Entre com o valor de depósito inicial: ");
+                valorSaldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            conta = new Conta(numConta, titular, valorSaldo);
+
+            System.Console.WriteLine("Dados da Conta: ");
+            System.Console.WriteLine(conta.ToString());
+
+            System.Console.Write("Entre com um valor para depósito: ");
+            double novoSaldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            conta.AdicionarNoSaldo(novoSaldo);
+
+            System.Console.WriteLine();
+            System.Console.WriteLine("Dados da conta atualizadas:");
+            System.Console.WriteLine(conta.ToString());
+
+            System.Console.Write("Entre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            conta.RemoverNoSaldo(saque);
+
+            System.Console.WriteLine();
+            System.Console.WriteLine("Dados da conta atualizados: ");
+            System.Console.WriteLine(conta.ToString());
+        }
+    }
+}
