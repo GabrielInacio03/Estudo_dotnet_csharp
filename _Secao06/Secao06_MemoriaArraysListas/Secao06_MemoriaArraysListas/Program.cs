1aba0e06e527088b3457b1b681499b69db6461c2
GabrielInacio03 <inaciogs03@gmail.com>
2023-05-27 11:59:16 -0300

    nullable e operador de coalescencia nula

--- _Secao06/Secao06_MemoriaArraysListas/Secao06_MemoriaArraysListas/Program.cs
+++ _Secao06/Secao06_MemoriaArraysListas/Secao06_MemoriaArraysListas/Program.cs
@@ -14,6 +14,25 @@ namespace Secao06_MemoriaArraysListas // Note: actual namespace depends on the p
 			Point p; //tipo valor, a simples declaração ja sera criada as caixinhas na memoria.
 			p.X = 10;
 			p.Y = 20;
+
+
+			double? x = null; //permite nulo no tipo valor (Nullable<double> ou double?)
+			double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault()); //pega o valor do x ou se não existir o valor padrõa, nesse caso o padrão seria 0
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue); //verifica se a variavel possui valor, retornando true ou false
+            Console.WriteLine(y.HasValue);
+
+			
+            Console.WriteLine((x.HasValue) ? x.Value : "x is null");
+            Console.WriteLine((y.HasValue) ? y.Value : "y is null");
+
+			//operador de coalescência nula
+			double? x2 = null;
+			double? y2 = x ?? 0.00; //se o valor de x não for nulo o y recebe o x, se não ele recebe o valor 0.00
+
             Console.WriteLine(p);
         }
 	}
