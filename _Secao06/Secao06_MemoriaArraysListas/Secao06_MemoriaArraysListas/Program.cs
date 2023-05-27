f25a301cbd46d5c754710b90c976b409205a9f65
GabrielInacio03 <inaciogs03@gmail.com>
2023-05-27 11:21:06 -0300

    struct

--- _Secao06/Secao06_MemoriaArraysListas/Secao06_MemoriaArraysListas/Program.cs
+++ _Secao06/Secao06_MemoriaArraysListas/Secao06_MemoriaArraysListas/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Drawing;
+
+namespace Secao06_MemoriaArraysListas // Note: actual namespace depends on the project name.
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			/*
+			 O struct tem como objetivo estruturar alguns dados comuns a um mesmo contexto, 
+			  bem parecida com a ideia de classes
+			 */
+			Point p; //tipo valor, a simples declaração ja sera criada as caixinhas na memoria.
+			p.X = 10;
+			p.Y = 20;
+            Console.WriteLine(p);
+        }
+	}
+}
