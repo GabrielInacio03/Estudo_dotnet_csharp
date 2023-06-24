f80f1f354bc38921dee6d4922034951ff6d77a19
GabrielInacio03 <inaciogs03@gmail.com>
2023-06-24 15:14:40 -0300

    trabalhando com arrays e matrizes

--- _Secao06/Secao06_MemoriaArraysListas/Secao06_MemoriaArraysListas/Program.cs
+++ _Secao06/Secao06_MemoriaArraysListas/Secao06_MemoriaArraysListas/Program.cs
@@ -8,7 +8,19 @@ namespace Secao06_MemoriaArraysListas // Note: actual namespace depends on the p
 	{
 		static void Main(string[] args)
 		{
-			System.Console.WriteLine("Ola Seção 06");	
+			System.Console.WriteLine("Ola Seção 06 - matrizes");
+			
+			//declarar matriz
+			double[,] mat = new double[2, 3]; // , indica que você ta criando um arranjo bidimensional com 2 linhas e 3 colunas
+
+			System.Console.WriteLine(mat.Length); //qtd de elementos a matriz tem no total
+
+			System.Console.WriteLine(mat.Rank); //primeira dimensão da matriz, que é a qtd de linhas
+
+			System.Console.WriteLine(mat.GetLength(0)); //dimensão 0 da matriz ela tem tamanho 2
+
+			System.Console.WriteLine(math.GetLength(1));
+
         }
 	}
 }
