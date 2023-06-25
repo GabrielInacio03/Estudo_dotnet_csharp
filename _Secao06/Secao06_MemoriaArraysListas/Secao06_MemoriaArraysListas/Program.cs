6a37db86734d895d453bfad420b193aeeabd53e3
GabrielInacio03 <inaciogs03@gmail.com>
2023-06-25 08:50:22 -0300

    matrizes, entendendo o básico

--- _Secao06/Secao06_MemoriaArraysListas/Secao06_MemoriaArraysListas/Program.cs
+++ _Secao06/Secao06_MemoriaArraysListas/Secao06_MemoriaArraysListas/Program.cs
@@ -8,18 +8,33 @@ namespace Secao06_MemoriaArraysListas // Note: actual namespace depends on the p
 	{
 		static void Main(string[] args)
 		{
-			System.Console.WriteLine("Ola Seção 06 - matrizes");
+			
+			int n = int.Parse(Console.ReadLine());
+
+			int[,] mat = new int[n, n];
+
+			for(int i = 0; i < n; i++)
+			{
+				string[] values = Console.ReadLine().Split(' ');
+				for(int j = 0; j < n; j++)
+				{
+					mat[i, j] = int.Parse(values[j]);
+					System.Console.WriteLine(mat[i,j]);
+				}
+			}
+			
+			//System.Console.WriteLine("Ola Seção 06 - matrizes");
 			
 			//declarar matriz
-			double[,] mat = new double[2, 3]; // , indica que você ta criando um arranjo bidimensional com 2 linhas e 3 colunas
+			//double[,] mat = new double[2, 3]; // , indica que você ta criando um arranjo bidimensional com 2 linhas e 3 colunas
 
-			System.Console.WriteLine(mat.Length); //qtd de elementos a matriz tem no total
+			//System.Console.WriteLine(mat.Length); //qtd de elementos a matriz tem no total
 
-			System.Console.WriteLine(mat.Rank); //primeira dimensão da matriz, que é a qtd de linhas
+			//System.Console.WriteLine(mat.Rank); //primeira dimensão da matriz, que é a qtd de linhas
 
-			System.Console.WriteLine(mat.GetLength(0)); //dimensão 0 da matriz ela tem tamanho 2
+			//System.Console.WriteLine(mat.GetLength(0)); //dimensão 0 da matriz ela tem tamanho 2
 
-			System.Console.WriteLine(math.GetLength(1));
+			//System.Console.WriteLine(math.GetLength(1));
 
         }
 	}
