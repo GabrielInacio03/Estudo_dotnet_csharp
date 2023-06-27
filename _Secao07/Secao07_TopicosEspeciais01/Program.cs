9df5bb1de0db48e10fee9fce034c030ddd1ab2dc
GabrielInacio03 <inaciogs03@gmail.com>
2023-06-27 06:06:12 -0300

    estrutura switch-case

--- _Secao07/Secao07_TopicosEspeciais01/Program.cs
+++ _Secao07/Secao07_TopicosEspeciais01/Program.cs
@@ -7,6 +7,23 @@
             //inferir no tipo das variáveis
             var x = 10;
             var y = "o valor é uma string";
+
+            //estrutura switch-case
+
+            int isBoy = 1;
+
+            switch (isBoy)
+            {
+                case 1:
+                    System.Console.WriteLine("é menino");
+                    break;
+                case 2:
+                    System.Console.WriteLine("é menina");
+                    break;
+                default:
+                    System.Console.WriteLine("é gemeos");
+                    break;
+            }
         }
     }
 }
