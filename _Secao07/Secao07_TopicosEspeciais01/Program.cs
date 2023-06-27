243262d187bc3c2e36c1297b20536e49c08c05a4
GabrielInacio03 <inaciogs03@gmail.com>
2023-06-27 05:59:05 -0300

    inferindo tipo de variáveis

---
+++ _Secao07/Secao07_TopicosEspeciais01/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Secao06_MemoriaArraysListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inferir no tipo das variáveis
+            var x = 10;
+            var y = "o valor é uma string";
+        }
+    }
+}
