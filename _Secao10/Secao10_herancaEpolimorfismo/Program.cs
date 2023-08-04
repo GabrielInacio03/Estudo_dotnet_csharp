acd3fd8175aef66b3c7fcb370301213b15411974
GabrielInacio03 <inaciogs03@gmail.com>
2023-08-03 23:30:01 -0300

    inicio do módulo 10

---
+++ _Secao10/Secao10_herancaEpolimorfismo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Secao10_herancaEpolimorfismo // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
