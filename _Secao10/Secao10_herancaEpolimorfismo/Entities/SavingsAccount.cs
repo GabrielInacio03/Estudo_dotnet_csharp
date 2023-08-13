de109750c645e5ebc4cf36a2b79cd8c5365edd0c
GabrielInacio03 <inaciogs03@gmail.com>
2023-08-13 19:59:11 -0300

    upcasting and downcasting

---
+++ _Secao10/Secao10_herancaEpolimorfismo/Entities/SavingsAccount.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Secao10_herancaEpolimorfismo.Entities
+{
+    public class SavingsAccount : Account
+    {
+        public double interestRate { get; set; } //taxa de juros
+
+        public SavingsAccount(){}
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            this.interestRate = interestRate;
+        }
+        public void UpdateBalance()
+        {
+            this.Balance = this.Balance * this.interestRate;
+        }
+    }
+}
--- _Secao10/Secao10_herancaEpolimorfismo/Program.cs
+++ _Secao10/Secao10_herancaEpolimorfismo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Secao10_herancaEpolimorfismo.Entities;
 
 namespace Secao10_herancaEpolimorfismo // Note: actual namespace depends on the project name.
 {
@@ -10,6 +11,37 @@ namespace Secao10_herancaEpolimorfismo // Note: actual namespace depends on the 
             /*
                 Herança: É um tipo de associação que permite que uma classe herde dados e comportamentos de outra. A vantagem é o reuso e polimorfismo. Sintaxe: : e base (referenciar a superclasse)
             */
+            //casting -> converter de um tipo para o outro
+            //Upcasting -> convertendo um objeto para uma variavel subclasse para superclasse, uso comum do polimorfismo
+            //Downcasting -> da superclasse para a subclasse
+
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.00);
+
+            //UPCASTING 
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.00);
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+            //DOWNCASTING
+            BusinessAccount acc4 = (BusinessAccount)acc2; //casting
+            acc4.Loan(100.0);
+
+           // BusinessAccount acc5 = (BusinessAccount)acc3; //casting //erro
+            if(acc3 is BusinessAccount) //verificando se a varivel é do tipo de uma classe especifica
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3; //casting //erro
+                BusinessAccount acc5 = acc3 as BusinessAccount; //casting
+                acc5.Loan(200.0);
+                System.Console.WriteLine("LOAN!!!!");
+            }
+
+            if(acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = (SavingsAccount)acc3;
+                acc5.UpdateBalance();
+                System.Console.WriteLine("FIZ UM UPDATE");
+            }
+
         }
     }
 }
