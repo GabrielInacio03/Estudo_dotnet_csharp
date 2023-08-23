ac6c09618fc0bdb5ee2381f901f9abfcfafb3fe7
GabrielInacio03 <inaciogs03@gmail.com>
2023-08-22 22:38:49 -0300

    tratando exceções

---
+++ _Secao11/Secao11_Excecoes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Secao11_Excecoes // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = 20;
+                int n2 = 0;
+
+                int result = n1 / n2;
+                System.Console.WriteLine($"Resultado: {result}");
+            }
+            catch (DivideByZeroException)
+            {
+                System.Console.WriteLine("Erro DevideByZeroException");
+            }
+            catch(FormatException e)
+            {
+                System.Console.WriteLine($"FormatException: {e.Message}");
+            }
+        }
+    }
+}
