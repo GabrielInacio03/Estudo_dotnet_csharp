c0b5803786ca2d5dab3fe3be3144c785cef8fb16
GabrielInacio03 <inaciogs03@gmail.com>
2023-10-29 10:39:31 -0300

    bloco using

--- _Secao13/Secao13_arquivos/Program.cs
+++ _Secao13/Secao13_arquivos/Program.cs
@@ -16,34 +16,26 @@ namespace Secao13_arquivos // Note: actual namespace depends on the project name
 
             string sourcePath = @"C:\workspace\Estudo_dotnet_csharp\_Secao13\Secao13_arquivos\arquivos\file1.txt";
             string targetPath = @"C:\workspace\Estudo_dotnet_csharp\_Secao13\Secao13_arquivos\arquivos\file2.txt";
-
-
-            //FileStream fs = null;
-            StreamReader sr = null;
+            
             try
             {
-                //recurso externo
-                //fs = new FileStream(sourcePath,FileMode.Open);   
-                sr = File.OpenText(sourcePath);//new StreamReader(fs);
                 
-                while(!sr.EndOfStream)
+                using (StreamReader sr = File.OpenText(sourcePath))
                 {
-                    string line = sr.ReadLine();
-                    System.Console.WriteLine("line: "+ line);
+                    while(!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        System.Console.WriteLine(line);
+                    }
                 }
                 
-               
             }
-            catch(IOException e)
+            catch (IOException e)
             {
                 System.Console.WriteLine("An error occurred");
                 System.Console.WriteLine(e.Message);
             }
-            finally
-            {
-                if(sr != null) sr.Close();
-                //if(fs != null) fs.Close();
-            }
+            
         }
     }
 }
