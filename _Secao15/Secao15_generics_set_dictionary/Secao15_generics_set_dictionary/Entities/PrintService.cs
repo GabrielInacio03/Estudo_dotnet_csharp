5a70a8dd6264fd6ca9356e0f0c10bce3fa71efe5
GabrielInacio03 <inaciogs03@gmail.com>
2023-11-29 11:20:45 -0300

    generics

---
+++ _Secao15/Secao15_generics_set_dictionary/Secao15_generics_set_dictionary/Entities/PrintService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secao15_generics_set_dictionary.Entities
+{
+    public class PrintService<T>
+    {
+        private T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T value)
+        {
+            if(_count == 10)
+            {
+                throw new Exception("PrintService is full");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+        public T First()
+        {
+            if (_count == 0)
+            {
+                throw new Exception("PrintService is empty");
+            }
+            return _values[0];
+        }
+        public void Print()
+        {
+            Console.WriteLine("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.WriteLine(_values[i] + ", ");
+            }
+            if(_count > 0) 
+            {
+                Console.WriteLine(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+            Console.WriteLine("First: "+ First());
+        }
+    }
+}
---
+++ _Secao15/Secao15_generics_set_dictionary/Secao15_generics_set_dictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using Secao15_generics_set_dictionary.Entities;
+using System;
+
+namespace Secao15_generics_set_dictionary // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> printService = new PrintService<int>();
+
+            Console.WriteLine("How many values ?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            printService.Print();
+        }
+    }
+}
