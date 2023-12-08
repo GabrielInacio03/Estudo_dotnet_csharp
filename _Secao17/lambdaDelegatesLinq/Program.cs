6ba9773777d17f9b4b4b20767c98947075194ca6
GabrielInacio03 <inaciogs03@gmail.com>
2023-12-07 21:27:53 -0300

    entendendo o delegate

---
+++ _Secao17/lambdaDelegatesLinq/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using lambdaDelegatesLinq.Services;
+
+namespace lambdaDelegatesLinq // Note: actual namespace depends on the project name.
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Max;
+
+            double result = op(a,b);
+            System.Console.WriteLine(result);
+
+        }
+    }
+}
