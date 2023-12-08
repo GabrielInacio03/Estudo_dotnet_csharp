04e57fdc30b9129f78a8a0576ce5facfbb92be9f
GabrielInacio03 <inaciogs03@gmail.com>
2023-12-07 21:34:47 -0300

    teste

--- _Secao15/Secao15_generics_set_dictionary/Secao15_generics_set_dictionary/Program.cs
+++ _Secao15/Secao15_generics_set_dictionary/Secao15_generics_set_dictionary/Program.cs
@@ -7,28 +7,39 @@ namespace Secao15_generics_set_dictionary // Note: actual namespace depends on t
     {
         static void Main(string[] args)
         {
-            //SortedSet mantem os elementos ordenados
-           SortedSet<int> a = new SortedSet<int>()
-           {
-               0,2,4,5,6,8,10
-           };
-           SortedSet<int> set = new SortedSet<int>()
-           {
-               5,6,7,8,9,10
-           };
-            //union
-            SortedSet<int> c = new SortedSet<int>(a);
-            c.UnionWith(set);
+            var time = DateTime.Now;
 
-            PrintCollection(c);
+            Console.WriteLine("Time: "+ time);
 
-            //intersection
-            SortedSet<int> d = new SortedSet<int>(a);
-            d.IntersectWith(set); //elementos q existem nos dois conjuntos
+            DateTime dataMinima = new DateTime(2000,01,01);
+            if (dataMinima.CompareTo(time) > 0)
+            {
+                Console.WriteLine("entrou");
+                time = DateTime.UtcNow;
+            }
+            Console.WriteLine("Time: " + time);
+            // //SortedSet mantem os elementos ordenados
+            //SortedSet<int> a = new SortedSet<int>()
+            //{
+            //    0,2,4,5,6,8,10
+            //};
+            //SortedSet<int> set = new SortedSet<int>()
+            //{
+            //    5,6,7,8,9,10
+            //};
+            // //union
+            // SortedSet<int> c = new SortedSet<int>(a);
+            // c.UnionWith(set);
 
-            //difference
-            SortedSet<int> e = new SortedSet<int>(a);
-            e.ExceptWith(set); //elementos de A excluido aqueles elementos de B
+            // PrintCollection(c);
+
+            // //intersection
+            // SortedSet<int> d = new SortedSet<int>(a);
+            // d.IntersectWith(set); //elementos q existem nos dois conjuntos
+
+            // //difference
+            // SortedSet<int> e = new SortedSet<int>(a);
+            // e.ExceptWith(set); //elementos de A excluido aqueles ele  mentos de B
         }
 
         static void PrintCollection<T>(IEnumerable<T> collection)
